8dc9666dd9d89fce858e7e11fae25863f41b1f94
gavin.lechner <gavin@pop-os.localdomain>
2020-10-23 19:54:31 -0400

    Add the list of addresses I will email and the beginning of the user creation script

---
+++ mailUsers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#if not root then exit
+
+#check if the csi230 group is created
+
+#if the emails arent already users Create usernames by grabbing the first section of the email
+
+#Create a random password using openssl for the user
+
+#create the account if it doesn exist with the username, password, and a home directory and #that the default bash 
+
+#make sure the user has to change their password when they first log in
+
+#send the user an email with their initial username/password google "send a gmail from bash 2fa"
+
+#delete the users for testing purposes
