fb10528e4d847ae26c3e8fd965173d251562318d
gavin.lechner <gavin@pop-os.localdomain>
2020-10-23 21:57:27 -0400

    Add a loop through the list of users and seperate them into username and create a simple password. Stuck on adding users

--- mailUsers.sh
+++ mailUsers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+USERNAME=" ";
+
 #check if the user is root
 if [ $USER != 'root' ]; then
    echo "Must run as root"    
@@ -10,10 +12,15 @@ if [ $(getent group CSI230) ]; then
    echo "CSI230 Is already a group"
 else
    echo "creating group CSI230"
+   groupadd CSI230
 fi
-
 #if the emails arent already users Create usernames by grabbing the first section of the email
-
+while read line
+do
+USERNAME=$line | head -n1 | cut -d "@" -f1
+PASSWORD=$(openssl passwd -1 "Password")
+useradd -m -p $PASSWORD $USERNAME
+done < 'addresses.txt'
 #Create a random password using openssl for the user
 
 #create the account if it doesn exist with the username, password, and a home directory and #that the default bash 
